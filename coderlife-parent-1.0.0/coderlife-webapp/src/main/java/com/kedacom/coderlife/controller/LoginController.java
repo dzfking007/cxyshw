d65554c5f4adf3cdd1a40709b11e14b7af6c3156
dzfking007 <dzfking007@163.com>
2013-07-31 10:36:26 +0800

    add

---
+++ coderlife-parent-1.0.0/coderlife-webapp/src/main/java/com/kedacom/coderlife/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.kedacom.coderlife.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.kedacom.coderlife.bussiness.service.UserDetailService;
+
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+	
+	@Resource
+	private UserDetailService userDetailService;
+	@RequestMapping(value="/login",method=RequestMethod.GET)
+	public String checkLogin(){
+		userDetailService.checkLogin();
+		return "Login";
+	}
+	
+	
+
+}
