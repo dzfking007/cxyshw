47bcc9a7c995d891837a4e3ff8be59f0fe380a42
yuxiaoguang <yuxg0215@126.com>
2013-07-31 15:42:48 +0800

    1111

--- coderlife-parent-1.0.0/coderlife-webapp/src/main/java/com/kedacom/coderlife/controller/LoginController.java
+++ coderlife-parent-1.0.0/coderlife-webapp/src/main/java/com/kedacom/coderlife/controller/LoginController.java
@@ -18,7 +18,7 @@ public class LoginController {
 	@RequestMapping(value="/login",method=RequestMethod.GET)
 	public String checkLogin(){
 		userDetailService.checkLogin();
-		return "Login1111";
+		return "Login1212";
 	}
 	
 	
