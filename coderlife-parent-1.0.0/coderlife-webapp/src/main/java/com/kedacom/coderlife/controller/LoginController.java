915d1f527fb8d789b82b12f6f64b095b0f23fdaa
yuxiaoguang <yuxg0215@126.com>
2013-08-01 15:08:01 +0800

    页面搭建

--- coderlife-parent-1.0.0/coderlife-webapp/src/main/java/com/kedacom/coderlife/controller/LoginController.java
+++ coderlife-parent-1.0.0/coderlife-webapp/src/main/java/com/kedacom/coderlife/controller/LoginController.java
@@ -1,12 +1,16 @@
 package com.kedacom.coderlife.controller;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.kedacom.coderlife.business.service.UserDetailService;
+import com.kedacom.coderlife.web.util.DirectlyRenderUtils;
 
 
 @Controller
@@ -15,12 +19,45 @@ public class LoginController {
 	
 	@Resource
 	private UserDetailService userDetailService;
-	@RequestMapping(value="/login",method=RequestMethod.GET)
-	public String checkLogin(){
-		userDetailService.checkLogin();
-		return "Login1212";
+	
+	/**
+	 * 登录
+	 * */
+	@RequestMapping(value="login",method=RequestMethod.POST)
+	public void checkLogin(HttpServletResponse response,String userName,String password){
+		JSONObject obj = new JSONObject();
+		try{
+//			userDetailService.checkLogin();
+			if(true){
+				obj.put("success", Boolean.TRUE);
+				obj.put("description","登录成功！");
+			}
+		}catch(Exception e){
+			obj.put("success", Boolean.FALSE);
+			obj.put("description", e.getMessage());
+			e.printStackTrace();
+		}finally{
+			DirectlyRenderUtils.renderJson(response,obj);
+		}
 	}
 	
+	/**
+	 * 注册
+	 * */
+	@RequestMapping(value="register",method=RequestMethod.POST)
+	public void registerUser(HttpServletResponse response,String userName,String password){
+		JSONObject obj = new JSONObject();
+		try{
+//			userDetailService.checkLogin();
+			obj.put("success", Boolean.TRUE);
+			obj.put("description","注册成功！");
+		}catch(Exception e){
+			obj.put("success", Boolean.FALSE);
+			obj.put("description", e.getMessage());
+			e.printStackTrace();
+		}finally{
+			DirectlyRenderUtils.renderJson(response,obj);
+		}
 	
-
+	}
 }
